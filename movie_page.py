cd52572f6ef6e7942f1181dba59d8d8626de6b2f
Manoj Prabhakar <manojprabhakar.isb@gmail.com>
2023-11-26 22:27:28 +0530

    Update movie_page.py

--- movie_page.py
+++ movie_page.py
@@ -3,10 +3,11 @@ import streamlit as st
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse import csr_matrix
 
+movies_data = pd.read_csv("movies.csv")
+
 def show_movie_based_page():
 	st.title("User Based Recommendation System")
 	train_data = pd.read_csv("train.csv")
-	movies_data = pd.read_csv("movies.csv")
 	user_movie_matrix = train_data.pivot(index='uID', columns='mID', values='rating').fillna(0)
 	movie_similarity = cosine_similarity(user_movie_matrix.T)
 	movie_similarity_df = pd.DataFrame(movie_similarity, index=user_movie_matrix.columns, columns=user_movie_matrix.columns)
@@ -18,7 +19,9 @@ def show_movie_based_page():
 	    top_movie_titles = movies_data[movies_data['mID'].isin(top_movie_indices)]['title']
 	    return top_movie_titles
 
-	movie_id = st.number_input('Movie id', min_value=1)  
-	recommendations = st.number_input('Number of Recommendations', min_value=1)  
-	recommended_movies = recommended_movies = get_movie_based_recommendations(movie_id, movie_similarity_df,recommendations)
-	st.write('Top Recommendations : ', recommended_movies)
+movielist = movies_data['title'].unique()
+moviename = st.selectbox("Movie Name", movielist)
+movienumber = movies_data[movies_data['title']=='moviename']
+recommendations = st.number_input('Number of Recommendations')  
+recommended_movies = recommended_movies = get_movie_based_recommendations(movienumber, movie_similarity_df,recommendations)
+st.write('Top Recommendations : ', recommended_movies)
