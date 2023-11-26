a70bd2826debfad3f6fc76c00d9236b8a1572b2e
Manoj Prabhakar <manojprabhakar.isb@gmail.com>
2023-11-26 22:16:57 +0530

    Update user_page.py

--- user_page.py
+++ user_page.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import csr_matrix
 
 def show_user_based_page():
 	st.title("User Based Recommendation System")
